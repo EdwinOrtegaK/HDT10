8a1f62c787c56cdb47e5d1f6e9c6bff23719fcfa
EdwinOrtegaK <andyortegakou@yahoo.com.mx>
2023-05-25 10:18:17 -0600

    Main edited

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        boolean bandera = true;
+        String ciu1;
+        String ciu2;
+        Integer tt;
 
         Graph myGraph = new Graph();
         myGraph.setNodes(Controller.getData());
@@ -9,12 +13,15 @@ public class Main {
 
         int[][] matrix = Matrix.adjacencyGenerator(myGraph);
         Controller.matrixIteration(matrix);
-
-
-        while (true){
-            System.out.println( );
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Ingrese una ciudad: ");
+            ciu1 = sc.nextLine();
+            System.out.println("Ingrese otra ciudad: ");
+            ciu2 = sc.nextLine();
+            System.out.println("Ingrese una tiempo: ");
+            tt = sc.nextInt();
+            bandera = true;
         }
-
-        }
-
     }
+}
