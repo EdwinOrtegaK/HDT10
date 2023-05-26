d09ea250d14804f8db4ec58a2cf140b6b896df58
EdwinOrtegaK <andyortegakou@yahoo.com.mx>
2023-05-25 19:06:01 -0600

    Floyd-Warshall

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         boolean bandera = true;
+        int[][] graph;
         String ciu1;
         String ciu2;
         Integer tt;
@@ -25,3 +26,22 @@ public class Main {
         }
     }
 }
+
+/*
+    FloydWarshall fw = new FloydWarshall();
+    int[][] shortestDistances = fw.floydWarshall(graph);
+
+// Imprimir la matriz con las distancias mínimas
+for (int i = 0; i < shortestDistances.length; i++) {
+        for (int j = 0; j < shortestDistances.length; j++) {
+        if (shortestDistances[i][j] == INF) {
+        System.out.print("INF\t");
+        } else {
+        System.out.print(shortestDistances[i][j] + "\t");
+        }
+        }
+        System.out.println();
+        }
+    CODIGO PARA PODER UTILIZAR EL ALGORITMO DE Floyd-Warshall
+ */
+
